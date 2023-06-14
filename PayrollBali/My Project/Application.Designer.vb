75ca0062ed8ff636ea497262cb2c2ebf321bb482
christoper12 <christoper.cbs@gmail.com>
2023-06-14 14:59:29 +0800

    push new -set stratup first form to login form

--- PayrollBali/My Project/Application.Designer.vb
+++ PayrollBali/My Project/Application.Designer.vb
@@ -31,7 +31,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = frmPayroll
+            Me.MainForm = frmLogin
         End Sub
     End Class
 End Namespace
